901fa0b1938d4fad44370a65140e106f54ff8584
Ben <y1999zk@gmail.com>
2019-04-29 01:27:12 +0100

    Add files via upload
    can show the goals and number live users to an html

--- bilibili_barrage.py
+++ bilibili_barrage.py
@@ -4,26 +4,46 @@ import requests, re
 import time
 import mysql.connector
 import datetime
+import codecs
 
 mydb = mysql.connector.connect(
-        host="119.27.165.***",
+        host="119.***.***.***",
         port="3306",
         user="********",
-        passwd="*******",
-        database="*****")
+        passwd="***********",
+        database="****")
 mycursor = mydb.cursor()
+goal = []
 
-def add(id,time2,content,roomid, token,headers):
+
+
+def whtml(qd):
+    f = codecs.open("/var/www/html/index.html", "w","utf-8")
+    s='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta http-equiv="refresh" content="10"><p ALIGN=Right style = "margin:0px"><font size="7" color="blue">当前签到人数:'+str(qd)+'</font></p>'
+    s=s+'<p ALIGN=Right style = "margin:0px"><font size="5" color="blue">发送以"目标"开头弹幕，设一个小目标吧！</font></p>'
+    s=s+'<p ALIGN=Right style = "margin:0px"><font size="5">'
+    for i in goal:
+        s=s+i+'<br>'
+    s=s+'</font></p>'
+    f.write(s)
+    f.close()
+
+def add(id,time2,content,roomid, token,headers,qd):
     try:
         sql=""
         finish = 0
         s = ""
         if content[0:2] == '目标':
             s = id + "设置成功，完成后别忘说‘完成’"
+            goal.append(id+content.replace('目标',''))
             finish = 1
-        elif content[0:2] == '完成':
+        elif content.find('完成')>=0:
             s = "恭喜" + id + "完成目标"
-            sql = "update barrage  set finish =  2 where id ='" + id + "'; "
+            sql = "update barrage  set finish =  2 where finish = 1 and id ='" + id + "'; "
+            for i in goal:
+                if i[:len(id)] == id:
+                    goal.remove(i)
+
             mycursor.execute(sql)
             mydb.commit()
             finish = 2
@@ -55,28 +75,35 @@ def add(id,time2,content,roomid, token,headers):
         except:
             print("exp" + sql)
 
-        if content.replace(' ', '') == '签到' or content.replace(' ', '') == '打卡':
+        if content.find('签到')>=0 or content.find('打卡')>=0:
             sql = "select time from biliid where id='" + id + "' limit 1 "
             mycursor.execute(sql)
             r = mycursor.fetchall()
 
             if r == [(None,)]:
+                # sql="insert into biliid (id,point, status, time)"
+                # sql=sql+"value('" + id + "',1,1,'" + time2 + "')"
                 sql = "update biliid  set point =  "
                 sql = sql + str(point + 1) + "  , status = 1 , time =  '" + time2 + "' where id ='" + id + "'; "
-                s = id + "签到成功，已经签到" + str(point + 1) + "次"
+                s = id + "签到成功，这是第" + str(point + 1) + "天来啦"
+
+                qd=qd+1
+
                 mycursor.execute(sql)
                 mydb.commit()
-            elif (datetime.datetime.now() - r[0][0] + datetime.timedelta(seconds=43200)).days > 0:
+            elif (datetime.datetime.now() - r[0][0] + datetime.timedelta(seconds=21600)).days > 0:
                 sql = "update biliid  set point =  "
                 sql = sql + str(point + 1) + "  , status = 1 , time =  '" + time2 + "' where id ='" + id + "'; "
-                s = id + "签到成功，已经签到" + str(point + 1) + "次"
+                s = id + "签到成功，这是第" + str(point + 1) + "天来啦"
+                qd = qd + 1
                 mycursor.execute(sql)
                 mydb.commit()
             else:
-                s = id + "今天签过到了，明天再来吧"
+                s = id + "今天签过了，明天再来吧"
         elif content.replace(' ', '') == '开始':
             sql = "update biliid  set status = 1, time = '" + time2+"'  where id ='" + id + "'; "
             s = id[:9] + "继续学习吧"
+            qd = qd + 1
             mycursor.execute(sql)
             mydb.commit()
 
@@ -98,12 +125,13 @@ def add(id,time2,content,roomid, token,headers):
                     s = "拜拜，明天见"
                     sql = "update biliid set total =  "
                     sql = sql + str(total) + " , status = 0 where id ='" + id + "'; "
+                    qd = qd - 1
                     mycursor.execute(sql)
                     mydb.commit()
         send(s, roomid, token, headers)
-    except:
-        print(sql+' '+s)
-    return s
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err)+'\n'+s+'\n'+sql)
+    return qd
 
 
 def send(barrage,roomid,token,headers):
@@ -116,7 +144,6 @@ def send(barrage,roomid,token,headers):
     i=0
     while i<l:
         s=barrage[i:i+19]
-        print(s)
         i=i+19
         time.sleep(1)
         data = {
@@ -147,15 +174,32 @@ def send(barrage,roomid,token,headers):
 
 
 def main():
-    url = 'https://live.bilibili.com/5290652'   # 
-    cookie ='***************************************************************'
+    qd=0
+    url = 'https://live.bilibili.com/5290652' 
+    cookie ='_uuid=****************; buvid3=**-*-*-*-*********; LIVE_BUVID=**********; sid=*; DedeUserID=*; DedeUserID__ckMd5=*; SESSDATA=*%*%***; bili_jct=*; _dfcaptcha=*'
     token = re.search(r'bili_jct=(.*?);', cookie).group(1)
     html = requests.get(url).text
+    # if re.search(r'room_id":(.*?),', html):
+    #     roomid = re.search(r'room_id":(.*?),', html).group(1)
+    #     print('直播房间号为:' + roomid)
+    # else:
+    #     print('抱歉,未找到此直播房间号~')
     roomid='5290652'
     l = []
     name=''
+    # t = datetime.datetime.strptime('Jun 1 2000', '%b %d %Y')
     t = datetime.datetime.now()
     time.sleep(5)
+
+    sql = "select id,content from barrage where finish =1"
+    mycursor.execute(sql)
+    r = mycursor.fetchall()
+    if r!=[]:
+        for i in r:
+            goal.append(i[0]+" "+i[1].replace('目标',''))
+
+    print(goal)
+    b = True
     while True:
         url = 'https://api.live.bilibili.com/ajax/msg'
         form = {
@@ -169,6 +213,8 @@ def main():
         }
         html = requests.post(url, data=form)
         result = html.json()['data']['room']
+        # add('test5', '2019-04-28 00:00:01', '签到', roomid, token, headers,qd)
+        # for test
         for i in result:
             time.sleep(1)
             t1 = datetime.datetime.strptime(str(i['timeline']), '%Y-%m-%d %H:%M:%S')
@@ -181,8 +227,12 @@ def main():
                 t=t1
                 name=name1
                 print(s)
-                if name1 !='****':
-                    barrage = add(name1, time2, content,roomid, token,headers)
+                if name1 !='zk的机器人':
+                    qd = add(name1, time2, content,roomid, token,headers,qd)
+                # if barrage!="":
+                    # send(s, roomid, token,headers)
+                    # print('--'+barrage)
+        whtml(qd)
         time.sleep(5)
 
 
