7239e143fa9e56d4518f51cb9a71ad2c2441254a
Ben <y1999zk@gmail.com>
2019-05-22 19:32:27 +0100

    Add files via upload

--- bilibili_barrage.py
+++ bilibili_barrage.py
@@ -7,11 +7,11 @@ import datetime
 import codecs
 
 mydb = mysql.connector.connect(
-        host="119.***.***.***",
+        host="**************",
         port="3306",
-        user="********",
-        passwd="***********",
-        database="****")
+        user="*******************",
+        passwd="*******************",
+        database="mp")
 mycursor = mydb.cursor()
 goal = []
 
@@ -19,12 +19,34 @@ goal = []
 
 def whtml(qd):
     f = codecs.open("/var/www/html/index.html", "w","utf-8")
-    s='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta http-equiv="refresh" content="10"><p ALIGN=Right style = "margin:0px"><font size="7" color="blue">当前签到人数:'+str(qd)+'</font></p>'
-    s=s+'<p ALIGN=Right style = "margin:0px"><font size="5" color="blue">发送以"目标"开头弹幕，设一个小目标吧！</font></p>'
-    s=s+'<p ALIGN=Right style = "margin:0px"><font size="5">'
+    # f = codecs.open("D:\\index.html", "w","utf-8")
+    # s='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta http-equiv="refresh" content="120"><p ALIGN=Right style = "margin:0px">'
+    # s=s+  '<table width="100%" ><tr><td width="25%"><p ALIGN=Right style = "margin:0px"><font size="7">'
+    # s=s+  '今日签到人数:'+str(qd)+'<br>弹幕发"签到"即可'
+    # s=s+'</font></p></td><td><marquee  direction="left"  behavior="scroll"  scrollamount="18"  scrolldelay="5"  loop="10"   ><p ALIGN=Right '
+    # s=s+'style = "margin:0px"><font  size="7" style="font-family:隶书" >'
+    # j=0
+    # ss=""
+    # for i in goal:
+    #     if j==10:
+    #         j=0
+    #         ss=ss+'  以"目标"开头发送弹幕，设一个小目标吧！     '
+    #     ss = ss + i + '    '
+    #     j = j + 1
+    # ss=ss.replace(' ','&nbsp;')
+    # s=s+ss[:int(len(ss)/2)]+'<br>'+ss[int(len(ss)/2+1):]
+    # s=s+'</font></p></marquee>'
+
+    s='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta http-equiv="refresh" content="120"><p ALIGN=Right style = "margin:0px"><p ALIGN=left style = "margin:0px"><font size="8">今日签到人数:'
+    s=s+str(qd)+'<br>"目标"开头发弹幕 设一个小目标吧！ </font></p><marquee direction="up"  behavior="scroll"  scrollamount="8"  scrolldelay="5"  loop="10" height="350"  ><p ALIGN=left style = "margin:0px"><font  size="7" style="font-family:隶书" >'
+    ss=''
     for i in goal:
-        s=s+i+'<br>'
-    s=s+'</font></p>'
+        ss = ss + i +'<br>'
+    # ss=ss.replace(' ','&nbsp;')
+    s=s+ss
+    s=s+'</font></p></marquee>'
+    # print(s)
+
     f.write(s)
     f.close()
 
@@ -33,11 +55,11 @@ def add(id,time2,content,roomid, token,headers,qd):
         sql=""
         finish = 0
         s = ""
-        if content[0:2] == '目标':
-            s = id + "设置成功，完成后别忘说‘完成’"
-            goal.append(id+content.replace('目标',''))
+        if content[0:2] == '目标' or content[1:3] == '目标':
+            s = id + "设置成功，完成目标后别忘了告诉主播，弹幕发‘完成’"
+            goal.append(id+content.replace(' ','').replace('：','').replace('目标',':'))
             finish = 1
-        elif content.find('完成')>=0:
+        elif content[0:2] == '完成':
             s = "恭喜" + id + "完成目标"
             sql = "update barrage  set finish =  2 where finish = 1 and id ='" + id + "'; "
             for i in goal:
@@ -76,22 +98,24 @@ def add(id,time2,content,roomid, token,headers,qd):
             print("exp" + sql)
 
         if content.find('签到')>=0 or content.find('打卡')>=0:
-            sql = "select time from biliid where id='" + id + "' limit 1 "
-            mycursor.execute(sql)
-            r = mycursor.fetchall()
+            # sql = "select time from biliid where id='" + id + "' limit 1 "
+            # sql = "select status from biliid where id='" + id + "' limit 1 "
+            # mycursor.execute(sql)
+            # r = mycursor.fetchall()
 
-            if r == [(None,)]:
+            # or r == [(None,)]
+            if rid==[] or rid[0][5]==None  :
                 # sql="insert into biliid (id,point, status, time)"
                 # sql=sql+"value('" + id + "',1,1,'" + time2 + "')"
                 sql = "update biliid  set point =  "
                 sql = sql + str(point + 1) + "  , status = 1 , time =  '" + time2 + "' where id ='" + id + "'; "
-                s = id + "签到成功，这是第" + str(point + 1) + "天来啦"
-
+                s = id + "签到成功，这是第1次签到，关注一波之后常来呀"
                 qd=qd+1
-
                 mycursor.execute(sql)
                 mydb.commit()
-            elif (datetime.datetime.now() - r[0][0] + datetime.timedelta(seconds=21600)).days > 0:
+
+            # elif (datetime.datetime.now() - r[0][0] + datetime.timedelta(seconds=21600)).days > 0:
+            elif rid[0][4]==0 :
                 sql = "update biliid  set point =  "
                 sql = sql + str(point + 1) + "  , status = 1 , time =  '" + time2 + "' where id ='" + id + "'; "
                 s = id + "签到成功，这是第" + str(point + 1) + "天来啦"
@@ -113,6 +137,8 @@ def add(id,time2,content,roomid, token,headers,qd):
                 s = id + "今天还没有签到呢"
             elif rid[0][4]==0:
                 s = id + "今天还没有签到呢"
+            elif rid[0][4]==2:
+                s = id + "已经拜拜过了"
             else:
                 t = str(datetime.datetime.now() - rid[0][5]).split(':')
                 if int(t[0]) > 12:
@@ -124,8 +150,8 @@ def add(id,time2,content,roomid, token,headers,qd):
                     send(s, roomid, token, headers)
                     s = "拜拜，明天见"
                     sql = "update biliid set total =  "
-                    sql = sql + str(total) + " , status = 0 where id ='" + id + "'; "
-                    qd = qd - 1
+                    sql = sql + str(total) + " , status = 2 where id ='" + id + "'; "
+                    # qd = qd - 1
                     mycursor.execute(sql)
                     mydb.commit()
         send(s, roomid, token, headers)
@@ -136,8 +162,7 @@ def add(id,time2,content,roomid, token,headers,qd):
 
 def send(barrage,roomid,token,headers):
     url_send = 'https://api.live.bilibili.com/msg/send'
-    barrage=barrage.replace('摸','*')
-    barrage = barrage.replace('撸', '*')
+    barrage=barrage.replace('摸','*').replace('撸', '*').replace('鸡鸡', '*')
     if barrage=='':
         return
     l=len(barrage)
@@ -174,9 +199,9 @@ def send(barrage,roomid,token,headers):
 
 
 def main():
-    qd=0
-    url = 'https://live.bilibili.com/5290652' 
-    cookie ='_uuid=****************; buvid3=**-*-*-*-*********; LIVE_BUVID=**********; sid=*; DedeUserID=*; DedeUserID__ckMd5=*; SESSDATA=*%*%***; bili_jct=*; _dfcaptcha=*'
+    qd=40
+    url = 'https://live.bilibili.com/5290652'   # input('请输出您要查看的直播房间网址链接:')
+    cookie ='*************************************************'
     token = re.search(r'bili_jct=(.*?);', cookie).group(1)
     html = requests.get(url).text
     # if re.search(r'room_id":(.*?),', html):
@@ -196,10 +221,15 @@ def main():
     r = mycursor.fetchall()
     if r!=[]:
         for i in r:
-            goal.append(i[0]+" "+i[1].replace('目标',''))
+            goal.append(i[0]+" "+i[1].replace(' ', '').replace('：', '').replace('目标',':'))
 
-    print(goal)
+    # print(goal)
     b = True
+    ti =0
+
+
+
+
     while True:
         url = 'https://api.live.bilibili.com/ajax/msg'
         form = {
@@ -213,27 +243,46 @@ def main():
         }
         html = requests.post(url, data=form)
         result = html.json()['data']['room']
-        # add('test5', '2019-04-28 00:00:01', '签到', roomid, token, headers,qd)
+
+        # abc = '''   '''
+        # t1 = 50
+        # a1 = abc.split('\n')
+
+        # for i in a1:
+        #     b = (i.split('：'))
+        #     print(b)
+        #     add(b[0], '2019-05-19 20:' + str(t1) + ':00', b[1], roomid, token, headers, qd)
+        #     t1 = t1 + 1
+        # 1 + atr
+
         # for test
+
+        # 1+shws
+
         for i in result:
             time.sleep(1)
             t1 = datetime.datetime.strptime(str(i['timeline']), '%Y-%m-%d %H:%M:%S')
-            name1=i['nickname']
-            if t1>t:   # or i['nickname'] != name
-                time2=i['timeline']
-                content=i['text']
+            name1 = i['nickname']
+            if t1 > t:  # or i['nickname'] != name
+                time2 = i['timeline']
+                content = i['text']
                 s = time2 + '[' + name1 + ']:' + content
                 l.append(s)
-                t=t1
-                name=name1
+                t = t1
+                name = name1
                 print(s)
-                if name1 !='zk的机器人':
-                    qd = add(name1, time2, content,roomid, token,headers,qd)
+                if name1 != 'zk的机器人':
+                    qd = add(name1, time2, content, roomid, token, headers, qd)
                 # if barrage!="":
-                    # send(s, roomid, token,headers)
-                    # print('--'+barrage)
-        whtml(qd)
+                # send(s, roomid, token,headers)
+                # print('--'+barrage)
+
         time.sleep(5)
+        ti = ti + 1
+        if ti == 10:
+            whtml(qd)
+            ti = 0
+
 
 
 
